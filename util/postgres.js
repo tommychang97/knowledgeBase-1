a332fc94acf7c15dfe0496056e41d702aa541cc2
tommychang97 <tommychang97@gmail.com>
2020-04-07 03:02:42 -0700

    new db

--- util/postgres.js
+++ util/postgres.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: '127.0.0.1',
-    user: 'postgres',
-    database: 'postgres',
-    password: 'sprite04051111',
+    host: 'ec2-34-197-212-240.compute-1.amazonaws.com',
+    user: 'pdqnqvrhrbcqhp',
+    database: 'd2a0td3uisfk76',
+    password: '5cee69a724c0080a1560914033ca87d0e0dde8f6bf3833008822bc9171a1f3e0',
     port: 5432,
-    ssl: false,
+    ssl: true,
 });
 
 module.exports = pool;
